665ee032fac78a72465b7a0b5e6ed0eab179621c
Wesley-Oliveira <wesley0leby@gmail.com>
2021-09-16 10:15:06 -0300

    Adding database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin123@todo.gke9r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
